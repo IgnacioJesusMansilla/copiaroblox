ae4b0bb555618153b16a2e3d707dad5624d89da7
NachoMansilla <knjasu098@gmail.com>
2024-12-01 17:16:44 -0300

    test

---
+++ js.rbxcdn.com/2b4411d7c8f6d83175e6184b1771401fedde85fe7a184e658993abed4827e5d1.js
@@ -0,0 +1,4 @@
+var Roblox = Roblox || {};
+Roblox.LangDynamic = Roblox.LangDynamic || {};
+Roblox.LangDynamic["Feature.EmailVerificationChallenge"] = {"Header.EnterCode":"กรอกรหัสยืนยันตัวตน","Header.VerifyYourAccount":"หากต้องการดำเนินการต่อ โปรดยืนยันอีเมลของคุณ","Description.SuspiciousActivityEmailVerification":"ขั้นตอนนี้เป็นการยืนยันว่าคุณเป็นมนุษย์","Description.EnterCode":"กรอกรหัสที่เราเพิ่งส่งไปยังอีเมลของคุณ","Header.ConfirmAbandon":"ออกจากการยืนยันตัวตนใช่ไหม?","Description.ConfirmAbandon":"หากออกจากขั้นตอนการยืนยันตัวตน คุณจะดำเนินการให้เสร็จไม่ได้","Label.ConfirmAbandon":"ออก","Label.RejectAbandon":"ย้อนกลับ"};
+window.Roblox && window.Roblox.BundleDetector && window.Roblox.BundleDetector.bundleDetected("DynamicLocalizationResourceScript_Feature.EmailVerificationChallenge");
---
+++ js.rbxcdn.com/f7a4b656b1ae68f6b3779e09307fa43d0117b23a355ca17df1d5274b8e5e1f04.js
@@ -0,0 +1,4 @@
+var Roblox = Roblox || {};
+Roblox.LangDynamic = Roblox.LangDynamic || {};
+Roblox.LangDynamic["Feature.PhoneVerificationChallenge"] = {"Description.ConfirmAbandon":"Si sales de la verificación, no podrás completar esta acción.","Label.RejectAbandon":"Volver","Label.ConfirmAbandon":"Salir","Header.ConfirmAbandon":"¿Salir de la verificación?"};
+window.Roblox && window.Roblox.BundleDetector && window.Roblox.BundleDetector.bundleDetected("DynamicLocalizationResourceScript_Feature.PhoneVerificationChallenge");
